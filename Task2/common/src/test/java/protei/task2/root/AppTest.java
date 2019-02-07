ec21c3b053ac2ccdc2589b2a8aedc7f31b66740c
Dyatlov Mikhail <neutralmike@yandex.ru>
2019-02-07 11:55:27 +0300

    Task2 and HttpServer from windows

---
+++ Task2/common/src/test/java/protei/task2/root/AppTest.java
@@ -0,0 +1,20 @@
+package protei.task2.root;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
